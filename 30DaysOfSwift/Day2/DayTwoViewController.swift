b6c441b3eaad72b6120c3b49a3a8c3a8432ee83a
yazzy4 <yazzy4@gmail.com>
2020-04-09 13:16:08 -0400

    Added tap and long press gestures to day 2

--- 30DaysOfSwift/Day2/DayTwoViewController.swift
+++ 30DaysOfSwift/Day2/DayTwoViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class DayTwoViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    let numberFormatter = NumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        numberFormatter.allowsFloats = false
+        self.title = "Gesture Tap Counter"
     }
     
-
+    @IBAction func didTapScreen(_ sender: UITapGestureRecognizer) {
+        incrimentCounter()
+    }
+    
+    
+    @IBAction func didHoldOnScreen(_ sender: UILongPressGestureRecognizer) {
+        incrimentCounter()
+    }
+    
+    func incrimentCounter(){
+        if let number = numberFormatter.number(from: counterLabel.text!){
+            counterLabel.text = "\(number.intValue + 1)"
+        }
+    }
 }
