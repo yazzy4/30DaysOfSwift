c2fd2d641fc88aed891d09601e800598d338f624
yazzy4 <yazzy4@gmail.com>
2020-04-11 18:42:23 -0400

    segment default and custom slider working

--- 30DaysOfSwift/Day4/DayFourViewController.swift
+++ 30DaysOfSwift/Day4/DayFourViewController.swift
@@ -13,6 +13,7 @@ class DayFourViewController: UIViewController {
     @IBOutlet weak var mainAmountField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     
     var numberFormatter = NumberFormatter()
     var tipModel = TipModel()
@@ -32,12 +33,36 @@ class DayFourViewController: UIViewController {
     }
 
     @IBAction func didChangeSegment(_ sender: UISegmentedControl) {
+    if let number = numberFormatter.number(from: mainAmountField.text!) {
+        tipModel.mainAmount = number.doubleValue
+        tipModel.tipPercentage = segmentToTipPercent(index: sender.selectedSegmentIndex)
+            applyTipsToUI(tipTouple: tipModel.calculateTipAmount())
+            
+            }
+    }
+    
+    @IBAction func didChangeSliderAmount(_ sender: UISlider){
+        if let number = numberFormatter.number(from:mainAmountField.text!) {
+            tipModel.mainAmount = number.doubleValue
+            tipModel.tipPercentage = Double(sender.value)
+                applyTipsToUI(tipTouple: tipModel.calculateTipAmount())
+                     
+            }
+        
+    }
+    
+    @IBAction func didEnterAmount(_ sender: UITextField){
         if let number = numberFormatter.number(from: mainAmountField.text!) {
             tipModel.mainAmount = number.doubleValue
-            tipModel.mainAmount = number.doubleValue
-            tipModel.tipPercentage = segmentToTipPercent(index: sender.selectedSegmentIndex)
-            
-        }
+            tipModel.tipPercentage = segmentToTipPercent(index: segmentControl.selectedSegmentIndex)
+            applyTipsToUI(tipTouple: tipModel.calculateTipAmount())
+                            
+            }
+    }
+    
+    func applyTipsToUI(tipTouple:(tipAmount:Double, totalAmount:Double)){
+        self.tipAmountLabel.text = String.init(format: "$%0.2f", tipTouple.tipAmount)
+        self.totalAmountLabel.text = String.init(format: "$%0.2f", tipTouple.totalAmount)
     }
     
     func segmentToTipPercent(index: Int) -> Double {
@@ -65,7 +90,7 @@ class TipModel {
             return(0.0,0.0)
         }
         //How do you calculate tip? total * percent then percent + main amount
-        return(mainAmount * tipPercentage, (mainAmount + tipPercentage) + tipPercentage)
+        return(mainAmount * tipPercentage, (mainAmount * tipPercentage) + mainAmount)
     }
     
 }
