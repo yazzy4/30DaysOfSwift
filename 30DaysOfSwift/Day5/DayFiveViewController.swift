b50de6c7283b6be883a50c0f5109f93875966492
yazzy4 <yazzy4@gmail.com>
2020-04-13 16:23:56 -0400

    Set up basic tableview

--- 30DaysOfSwift/Day5/DayFiveViewController.swift
+++ 30DaysOfSwift/Day5/DayFiveViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class DayFiveViewController: UIViewController {
+class DayFiveViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.title = "Table View"
 
     }
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "Row # \(indexPath.row)"
+        
+        return cell!
+    }
 
 
 }
