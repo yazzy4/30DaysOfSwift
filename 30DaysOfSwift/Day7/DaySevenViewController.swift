419c6e7578dc25322f8c0fd0c7d60f4903f3f7e9
yazzy4 <yazzy4@gmail.com>
2020-04-14 13:59:10 -0400

    Set up segues between storyboards

--- 30DaysOfSwift/Day7/DaySevenViewController.swift
+++ 30DaysOfSwift/Day7/DaySevenViewController.swift
@@ -9,19 +9,41 @@
 import UIKit
 
 class DaySevenViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "First View"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(didPressDoneButton(_:)))
 
     }
     
+    @objc func didPressDoneButton(_ sender: UIBarButtonItem){
+        performSegue(withIdentifier: DaySevenDetailViewController.identifier, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == DaySevenDetailViewController.identifier {
+            let vc = segue.destination as! DaySevenDetailViewController
+            vc.text = self.textView.text!
+        }
+    }
 
 }
 
 class DaySevenDetailViewController: UIViewController {
 
+    static let identifier = "day7Detail"
+    var text: String?
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Second View"
+        textView.text = text
 
     }
     
