aa63ddac0997f8f1a7bac2d175f96f621f86fecf
yazzy4 <yazzy4@gmail.com>
2020-04-14 13:33:29 -0400

    Setup files and storyboards for Day 7 - Passing Data

---
+++ 30DaysOfSwift/Day7/DaySevenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DaySevenViewController.swift
+//  30DaysOfSwift
+//
+//  Created by Yaz Burrell on 4/14/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class DaySevenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
+
+class DaySevenDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
