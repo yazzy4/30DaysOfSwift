cfde0c637025b42205650dc314d972c7f03a1fca
yazzy4 <yazzy4@gmail.com>
2020-04-15 15:14:29 -0400

    Added color to refresh control, created sample input to append to list when refresh is called, added title to refresh for user feedback

--- 30DaysOfSwift/Day10/DayTenViewController.swift
+++ 30DaysOfSwift/Day10/DayTenViewController.swift
@@ -21,6 +21,20 @@ class DayTenViewController: UIViewController {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        self.tableView.refreshControl = refreshControl
+        self.refreshControl.addTarget(self, action: #selector(didRefreshView(_:)), for: UIControl.Event.valueChanged)
+        refreshControl.tintColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        refreshControl.attributedTitle = NSAttributedString(string: "Fetching new data...")
+    }
+    
+    //fetches hard coded data for refresh control
+    //
+    @objc func didRefreshView(_ sender: UIRefreshControl){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.data.append(contentsOf: ["Pears", "Blueberries", "Peaches", "Onions"])
+            self.refreshControl.endRefreshing()
+            self.tableView.reloadData()
+        }
     }
     
 }
