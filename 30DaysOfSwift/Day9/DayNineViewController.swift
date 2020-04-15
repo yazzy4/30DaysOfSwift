23ac14b47f5e614a6f54f3283720857e83c9e595
yazzy4 <yazzy4@gmail.com>
2020-04-15 12:23:27 -0400

    set up image picker in view controller

--- 30DaysOfSwift/Day9/DayNineViewController.swift
+++ 30DaysOfSwift/Day9/DayNineViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class DayNineViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    lazy var imagePicker: UIImagePickerController = {
+        let ip = UIImagePickerController()
+        ip.delegate = self
+        ip.allowsEditing = false
+        ip.sourceType = .photoLibrary
+        return ip
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Display Photo"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.camera, target: self, action: #selector(didTapCameraButton(_:)))
     }
     
+    @objc func didTapCameraButton(_ sender: UIBarButtonItem){
+        present(imagePicker, animated: true, completion: nil)
+            
+        
+    }
 
+}
 
+extension DayNineViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        self.imageView.image = pickedImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
