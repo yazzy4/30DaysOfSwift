85c2e1f8405b7040b908956249c6e141f692a165
yazzy4 <yazzy4@gmail.com>
2020-04-20 14:24:07 -0400

    added day 12  view controllers, table view and text view. Segue is working between two views

---
+++ 30DaysOfSwift/Day12/DayTwelveViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DayTwelveViewController.swift
+//  30DaysOfSwift
+//
+//  Created by Yaz Burrell on 4/20/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class DayTwelveViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+       
+     var data = ["Milk", "Apples", "Chicken", "Salt", "Eggs"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        //add bar button
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addNewItem(_:)))
+    }
+    
+    @objc func addNewItem(_ sender: UIBarButtonItem){
+        performSegue(withIdentifier: DayTwelveAddViewController.identifier, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == DayTwelveAddViewController.identifier {
+            let vc = segue.destination as! DayTwelveAddViewController
+        }
+    }
+
+}
+
+extension DayTwelveViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = data[indexPath.row]
+        return cell!
+    }
+    
+    
+}
