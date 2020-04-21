0124dddf1ae14f23ff63fad977aad074d01d061d
yazzy4 <yazzy4@gmail.com>
2020-04-21 13:17:08 -0400

    added cancel function to keyboard/view controller

--- 30DaysOfSwift/Day14/DayFourteenViewController.swift
+++ 30DaysOfSwift/Day14/DayFourteenViewController.swift
@@ -15,11 +15,22 @@ class DayFourteenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Keyboard Accessories"
+        
+        //add left nav bar item
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.cancel, target: self, action: #selector(dismissVC))
+        
+        //right nav bar item
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: nil, action: nil)
     }
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         textView.becomeFirstResponder()
+    }
+    
+    @objc func dismissVC(){
+        self.navigationController?.popViewController(animated: true)
     }
 
 }
