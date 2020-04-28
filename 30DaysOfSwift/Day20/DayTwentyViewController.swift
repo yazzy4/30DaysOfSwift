b90490d968b76fa93d8b827a76ebea56ddc3b75e
yazzy4 <yazburrell@pursuit.org>
2020-04-28 12:46:39 -0400

    Added container view to Day 20

---
+++ 30DaysOfSwift/Day20/DayTwentyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DayTwentyViewController.swift
+//  30DaysOfSwift
+//
+//  Created by Yaz Burrell on 4/28/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class DayTwentyViewController: UIViewController {
+    
+    @IBOutlet weak var containerView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
