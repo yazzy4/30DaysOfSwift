9561428e3101c8ae41683303bd017a3d862c2525
yazzy4 <yazburrell@pursuit.org>
2020-04-29 12:04:15 -0400

    Set up basic collection view for day 21

---
+++ 30DaysOfSwift/Day21/Day21Cell.swift
@@ -0,0 +1,16 @@
+//
+//  Day21Cell.swift
+//  30DaysOfSwift
+//
+//  Created by Yaz Burrell on 4/29/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class Day21Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageV: UIImageView!
+    
+    
+}
---
+++ 30DaysOfSwift/Day21/DayTwentyOneViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DayTwentyOneViewController.swift
+//  30DaysOfSwift
+//
+//  Created by Yaz Burrell on 4/28/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class DayTwentyOneViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var data = [UIImage]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Collection View Example"
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.populateData()
+    }
+
+    func populateData(){
+        //26 sample images total
+        for i in 0...26 {
+            data.append(UIImage(named: "image \(i)")!)
+        }
+    }
+
+}
+
+extension DayTwentyOneViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Day21Cell
+        cell.imageV.image = data[indexPath.item]
+        return cell
+    }
+    
+    
+}
