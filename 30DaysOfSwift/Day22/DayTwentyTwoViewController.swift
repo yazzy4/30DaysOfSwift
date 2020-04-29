38578d1e49295bc3dc2140e1a8b959cc95e4a3fc
yazzy4 <yazburrell@pursuit.org>
2020-04-29 13:55:13 -0400

    Schedule notification button, default button is working

--- 30DaysOfSwift/Day22/DayTwentyTwoViewController.swift
+++ 30DaysOfSwift/Day22/DayTwentyTwoViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import UserNotifications
 
-class DayTwentyTwoViewController: UIViewController {
+class DayTwentyTwoViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +19,29 @@ class DayTwentyTwoViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let center = UNUserNotificationCenter.current()
+        center.delegate = self
         center.requestAuthorization(options: [UNAuthorizationOptions.alert, .sound, .badge]) { (success, error) in
             
         }
     }
     
     @IBAction func didTapNotification(_ sender: UIButton){
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = "New Course Announcement"
+        content.subtitle = "30 Days of iOS 12"
+        content.body = "Learn iOS 12 in 30 Days"
+        content.sound = UNNotificationSound.default
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2.0, repeats: false)
+        let request = UNNotificationRequest(identifier: "ContentIdentifier", content: content, trigger: trigger)
+        
+        center.add(request) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+                
+            }
+        }
+   
         
     }
 
