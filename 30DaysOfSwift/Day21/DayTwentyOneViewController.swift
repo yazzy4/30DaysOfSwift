76b5f50b478a5170c2e1bcd88015751224a365c3
yazzy4 <yazburrell@pursuit.org>
2020-04-29 12:40:18 -0400

    populated collection view cells, fixed formatting issue

--- 30DaysOfSwift/Day21/DayTwentyOneViewController.swift
+++ 30DaysOfSwift/Day21/DayTwentyOneViewController.swift
@@ -17,15 +17,15 @@ class DayTwentyOneViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Collection View Example"
-        self.collectionView.dataSource = self
         self.collectionView.delegate = self
+        self.collectionView.dataSource = self
         self.populateData()
     }
 
     func populateData(){
         //26 sample images total
         for i in 0...26 {
-            data.append(UIImage(named: "image \(i)")!)
+            data.append(UIImage(named: "image\(i)")!)
         }
     }
 
@@ -35,6 +35,7 @@ extension DayTwentyOneViewController: UICollectionViewDataSource, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return data.count
     }
+
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Day21Cell
@@ -42,5 +43,9 @@ extension DayTwentyOneViewController: UICollectionViewDataSource, UICollectionVi
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSize = collectionView.bounds.size.width / 3
+        return CGSize(width: itemSize, height: itemSize)
+    }
     
 }
