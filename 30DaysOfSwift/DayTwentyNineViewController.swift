aeb15c0195ba00416956d7f32366676b0ef9d919
yazzy4 <yazburrell@pursuit.org>
2020-05-07 17:18:01 -0400

    Added tap gesture recognizer to shuffle language array

--- 30DaysOfSwift/DayTwentyNineViewController.swift
+++ 30DaysOfSwift/DayTwentyNineViewController.swift
@@ -12,12 +12,22 @@ class DayTwentyNineViewController: UIViewController {
     
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    var data = ["¿Hola cómo estás hoy?", "Hei, kuinka voit tänään?", "Ciao, come va oggi?", "오늘 어떻게 지내니?", "こんにちは今日はどのようにしていますか", "வணக்கம் நீங்கள் இன்று எப்படி உள்ளீர்கள்?", "你好，今天好嗎？"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.textView.text = ""
+        self.textView.text = data[0]
         self.resultLabel.text = ""
-        title = "Language Detector"
+        title = "NLP Demo"
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(randomString))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+   @objc func randomString(){
+        let random = Int.random(in: 0..<data.count)
+        textView.text = data[random]
     }
    
     @IBAction func detectLang(_ sender: UIButton){
