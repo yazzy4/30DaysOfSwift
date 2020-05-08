ccb68092dcf6667619251b342b42ef07efebe256
yazzy4 <yazburrell@pursuit.org>
2020-05-08 14:45:49 -0400

    Set up view controller for image recognition - day 30

---
+++ 30DaysOfSwift/Day30/DayThirtyViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DayThirtyViewController.swift
+//  30DaysOfSwift
+//
+//  Created by Yaz Burrell on 5/7/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+import ImageIO
+
+class DayThirtyViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
+
+extension CGImagePropertyOrientation {
+    
+}
