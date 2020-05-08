5d4436a16b6a618d373f44422b76ddd6473b5e6b
yazzy4 <yazburrell@pursuit.org>
2020-05-08 17:03:11 -0400

    Image detection working and is odd but cool - Done with day 30

--- 30DaysOfSwift/Day30/DayThirtyViewController.swift
+++ 30DaysOfSwift/Day30/DayThirtyViewController.swift
@@ -13,7 +13,7 @@ import Vision
 
 class DayThirtyViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageV: UIImageView!
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var classificationLabel: UILabel!
     
@@ -35,19 +35,61 @@ class DayThirtyViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Core ML Vision Example"
+        cameraButton.addTarget(self, action: #selector(selectPhoto(_:)), for: UIControl.Event.touchUpInside)
+    }
+    
+   @objc func selectPhoto(_ sender: UIButton){
+        let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    imagePicker.sourceType = .photoLibrary
+    present(imagePicker, animated: true)
+    
     }
     
     func classification(for request: VNRequest, error: Error?){
         DispatchQueue.main.async {
-            guard let classResult = request.results else {
+            guard let classResult = request.results, let classifications = classResult as? [VNClassificationObservation], classifications.count != 0  else  {
                 self.classificationLabel.text = "Unable to classify"
                 return
+            }
+            let topResults = classifications.prefix(2)
+            let description = topResults.map({ classificationObservation in
+                return String(format: "(%.2f) %@", classificationObservation.confidence, classificationObservation.identifier)
+            })
+            self.classificationLabel.text = description.joined(separator: "\n")
+        }
+        
+    }
+    
+    func processImage(image: UIImage) {
+        classificationLabel.text = "Working..."
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let cIImage = CIImage(image: image) else {
+            fatalError("Unable to process image")
+        }
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: cIImage, orientation: orientation)
+            do{
+                try handler.perform([self.classRequest])
+            }
+            catch {
+                fatalError("Failed to do classification")
             }
         }
     }
 
 
+}
+
+extension DayThirtyViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageV.image = image
+        processImage(image: image)
+        
+    }
 }
 
 extension CGImagePropertyOrientation {
